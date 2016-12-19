464098a43224493032ed89ee69e1e2ec10133b48
Luis Pujols <luispujols@hotmail.com>
2016-12-19 00:37:57 -0400

    UPDATE "Added the add icon to the floating button, and removed one section of the ViewPager, also modified some titles"

--- app/src/main/java/com/pujolsluis/android/hangeo/PlanListFragment.java
+++ app/src/main/java/com/pujolsluis/android/hangeo/PlanListFragment.java
@@ -31,7 +31,7 @@ public class PlanListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         RecyclerView rv = (RecyclerView) inflater.inflate(
-                R.layout.fragment_cheese_list, container, false);
+                R.layout.fragment_plan_list, container, false);
         setupRecyclerView(rv);
         return rv;
     }
