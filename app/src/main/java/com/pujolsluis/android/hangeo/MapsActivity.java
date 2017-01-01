6488738482e44ffc671fbaca83fa56ae398b4700
Luis Pujols <luispujols@hotmail.com>
2017-01-01 18:09:23 -0400

    ADD 'Added the Search Bar for the Map Activity, now the app searches for places succesfully, and moves the camera to the new place'

--- app/src/main/java/com/pujolsluis/android/hangeo/MapsActivity.java
+++ app/src/main/java/com/pujolsluis/android/hangeo/MapsActivity.java
@@ -2,29 +2,68 @@ package com.pujolsluis.android.hangeo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.multidex.MultiDex;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
 
-    private GoogleMap mMap;
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, PlaceSelectionListener {
+
+
     private DrawerLayout mDrawerLayout;
     Context context = this;
+
+    //Identifier for the Location Request
+    private static final int REQUEST_LOCATION = 0;
+    // Google Map Object
+    private GoogleMap mGoogleMap;
+    //Map Ready Indicator
+    private boolean mapReady = false;
+    //View to get the main activity layout and use it in the permission method
+    private View mLayout;
+    //Camera Position
+    private CameraPosition USER_LOCATION;
+    //Google API Cliente to retrieve user location data
+    private GoogleApiClient mGoogleApiClient;
+    //User Last Location
+    private Location mLastLocation;
+    private LatLng mLastLocationLatLng;
+    //FirstOnStart
+    private Boolean FirstonStart = true;
+    //Last Marker on long click
+    private Marker mLastMarker;
+    //PolyLine Between created markers
+    private Polyline mPolyLine;
+
+    private Marker mLastPlaceMarker;
+
+    private ArrayList<Marker> mLastMarkerslist = new ArrayList<>();
+    private ArrayList<LatLng> mPolyLinePointList = new ArrayList<>();
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -37,6 +76,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+/*  Old Toolbar for the layout [SOON TO BE REMOVED]
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,7 +85,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             ab.setHomeAsUpIndicator(R.drawable.ic_menu);
             ab.setDisplayHomeAsUpEnabled(true);
         }
-
+*/
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_map);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_map);
@@ -56,33 +96,57 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         navigationView.setCheckedItem(R.id.nav_home);
 
+        //CODE FOR MAP STARTS HERE
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Get Map Activity Layout
+        mLayout = findViewById(R.id.map_main_content);
+
+        //Set home button OnClickListener for the map Place Fragment
+        Button homeMenuButton = (Button) findViewById(R.id.home_menu_map_button);
+        homeMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+        // Retrieve the PlaceAutocompleteFragment.
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Register a listener to receive callbacks when a place has been selected or an error has
+        // occurred.
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+
+        //Initialize USER_LOCATION
+        mLastLocationLatLng = new LatLng(18.6976745, -71.2865409);
+        USER_LOCATION = CameraPosition.builder()
+                .target(mLastLocationLatLng)
+                .zoom((float) 15)
+                .bearing(0)
+                .build();
+
+
 
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //This Method gets called when the map is ready to execute
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        mGoogleMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sevendips = new LatLng(18.487438, -69.961872);
-        mMap.addMarker(new MarkerOptions().position(sevendips).title("Marker in sevendips"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sevendips));
-        mMap.setMinZoomPreference((float)16.5);
+        mGoogleMap.addMarker(new MarkerOptions().position(sevendips).title("Marker in sevendips"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sevendips));
+        mGoogleMap.setMinZoomPreference((float)16.5);
     }
 
     private void setupDrawerContent(final NavigationView navigationView) {
@@ -114,5 +178,40 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Method to get Project resources
+    public Resources getResources() {
+        return super.getResources();
+    }
+
+    @Override
+    public void onPlaceSelected(Place place) {
+        LatLng target = place.getLatLng();
+        MarkerOptions selectedPlaceMarker = new MarkerOptions()
+                .position(target)
+                .title(place.getName().toString() + " " + place.getPriceLevel());
+        Marker placeMarker = mGoogleMap.addMarker(selectedPlaceMarker);
+        mLastMarkerslist.add(placeMarker);
+        mLastPlaceMarker = placeMarker;
+//        mPolyLinePointList.add(placeMarker.getPosition());
+//        mPolyLine.setPoints(mPolyLinePointList);
+        CameraPosition newPosition = CameraPosition.builder()
+                .target(target)
+                .zoom(17)
+                .bearing(0)
+                .build();
+        flyTo(newPosition);
+
+    }
+
+    @Override
+    public void onError(Status status) {
+        return;
+    }
+
+    //This Method moves the camera to the position indicated with an animation
+    public void flyTo(CameraPosition cameraPosition) {
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
