5f678a7d27463b42f475b1b805feb765edc7ab5a
nelcolon <ncolonmeran@outlook.com>
2017-01-11 21:57:21 -0500

    Adding Forgot password activity to project

---
+++ app/src/main/java/com/pujolsluis/android/hangeo/ForgotPassword.java
@@ -0,0 +1,76 @@
+package com.pujolsluis.android.hangeo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private EditText inputMail;
+    private Button resetPasswordButton;
+    private Button goBackButton;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password_layout);
+
+        inputMail = (EditText) findViewById(R.id.editText_forgotPassword_email);
+        resetPasswordButton = (Button) findViewById(R.id.btn_reset_password);
+        goBackButton = (Button) findViewById(R.id.btn_back);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        auth = FirebaseAuth.getInstance();
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputMail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ForgotPassword.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ForgotPassword.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/pujolsluis/android/hangeo/SignUpActivity.java
+++ app/src/main/java/com/pujolsluis/android/hangeo/SignUpActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,13 +19,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText inputEmail, inputPassword ;
-    private Button buttonSignUp;
+    private Button buttonSignUp, buttonForgotPassword;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +36,45 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         //Get instance for firebase authentification
         firebaseAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null) {
+//                    // User is signed in
+//                    Toast.makeText(SignUpActivity.this, "onAuthStateChanged:signed_in", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    // User is signed out
+//                    Toast.makeText(SignUpActivity.this, "onAuthStateChanged:signed_out", Toast.LENGTH_SHORT).show();
+//                }
+                // ...
+            }
+        };
 
         buttonSignUp = (Button) findViewById(R.id.button_SignUpLayout_SignUp);
+        buttonForgotPassword = (Button) findViewById(R.id.btn_reset_password); 
+        
         inputEmail = (EditText) findViewById(R.id.editText_SignUpLayout_emailInput);
         inputPassword = (EditText) findViewById(R.id.editText_SignUpLayout_passwordInput);
         progressDialog = new ProgressDialog(this);
+        
         buttonSignUp.setOnClickListener(this);
+        buttonForgotPassword.setOnClickListener(this);
+        
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -47,24 +82,44 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         if (v == buttonSignUp){
             registerUser();
         }
+        
+        if (v == buttonForgotPassword){
+            goToForgotPassword();
+        }
+
+        /*if (v == buttonLogin){
+            goToLogin();
+        }*/
     }
+
+    private void goToForgotPassword() {
+            startActivity(new Intent(SignUpActivity.this, ForgotPassword.class));
+    }
+
+   /* private void goToLogin() {
+        startActivity(new Intent(SignUpActivity.this, ForgotPassword.class));
+    }*/
+
+
 
     private void registerUser() {
         String email = inputEmail.getText().toString().trim();
         String password = inputPassword.getText().toString().trim();
 
-        if (TextUtils.isEmpty(email)){
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if (TextUtils.isEmpty(password)){
-            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        progressDialog.setMessage("Registering user ...");
-        progressDialog.show();
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         firebaseAuth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -74,7 +129,6 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                             Toast.makeText(SignUpActivity.this, "Registered successfully!", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(SignUpActivity.this, "Could not register, please try again!", Toast.LENGTH_SHORT).show();
-                            progressDialog.hide();
                         }
                     }
                 });
