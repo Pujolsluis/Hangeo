fbb987684407c97d553f97436d73b3c324bb0efe
Luis Pujols <luispujols@hotmail.com>
2017-01-22 18:52:23 -0400

    ADD 'Created the time, and estimated cost views. also added the rating bar for the plan details activity'

--- app/src/main/java/com/pujolsluis/android/hangeo/PlanDetailsActivity.java
+++ app/src/main/java/com/pujolsluis/android/hangeo/PlanDetailsActivity.java
@@ -1,13 +1,20 @@
 package com.pujolsluis.android.hangeo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +23,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.pujolsluis.android.hangeo.R.id.plan_details_map;
 
@@ -48,6 +58,22 @@ public class PlanDetailsActivity extends AppCompatActivity implements OnMapReady
 
         loadBackdrop();
 
+        List<String> locations = new ArrayList<String>();
+        locations.add("Seven Dip's");
+        locations.add("Shot's Bar");
+        locations.add("Jet Set");
+        locations.add("Bowling Center");
+        locations.add("Boca Tabu Concert");
+
+        //Code for Locations List View
+        LocationsAdapter locationsAdapter = new LocationsAdapter(this, locations);
+
+        ListView listView = (ListView) findViewById(R.id.plan_details_locations_list_view);
+        listView.setAdapter(locationsAdapter);
+
+        listView.setDivider(null);
+        setListViewHeightBasedOnItems(listView);
+
         //CODE FOR MAP STARTS HERE
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -57,6 +83,7 @@ public class PlanDetailsActivity extends AppCompatActivity implements OnMapReady
 
         //Get Map Activity Layout
         mLayout = findViewById(R.id.main_content_plan_details);
+
     }
 
     private void loadBackdrop() {
@@ -92,4 +119,63 @@ public class PlanDetailsActivity extends AppCompatActivity implements OnMapReady
     public void flyTo(CameraPosition cameraPosition) {
         mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
+
+
+    public class LocationsAdapter extends ArrayAdapter<String> {
+        public LocationsAdapter(Context context, List<String> users) {
+            super(context, 0, users);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            String location = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.plan_details_locations_list_item, parent, false);
+            }
+            // Lookup view for data population
+            TextView locationName = (TextView) convertView.findViewById(R.id.plan_details_locations_list_item_TextView);
+
+            // Populate the data into the template view using the data object
+            locationName.setText(location);
+            // Return the completed view to render on screen
+            return convertView;
+        }
+    }
+
+
+    public static boolean setListViewHeightBasedOnItems(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter != null) {
+
+            int numberOfItems = listAdapter.getCount();
+
+            // Get total height of all items.
+            int totalItemsHeight = 0;
+            for (int itemPos = 0; itemPos < numberOfItems; itemPos++) {
+                View item = listAdapter.getView(itemPos, null, listView);
+                item.measure(0, 0);
+                totalItemsHeight += item.getMeasuredHeight();
+            }
+
+            // Get total height of all item dividers.
+            int totalDividersHeight = listView.getDividerHeight() *
+                    (numberOfItems - 1);
+
+            // Set list height.
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalItemsHeight + totalDividersHeight + 24;
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+
 }
