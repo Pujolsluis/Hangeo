a40ccd365c9ada9e35b9ceb2cad5ba8ad8553b50
Luis Pujols <luispujols@hotmail.com>
2017-04-19 18:37:45 -0400

    ADD 'Created the UnderConsActivity'

---
+++ app/src/main/java/com/pujolsluis/android/hangeo/UnderConsActivity.java
@@ -0,0 +1,17 @@
+package com.pujolsluis.android.hangeo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Oficina on 19/04/2017.
+ */
+
+public class UnderConsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
